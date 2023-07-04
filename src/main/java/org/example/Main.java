cddaa2562b4589152303be3a3fc5b3e5c2d26d0e
UnknownRussian <aliaksandr.sanin@gmail.com>
2023-07-04 16:20:32 +0200

    Notes for app

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,77 @@
+package org.example;
+/***
+ MUST HAVE:
+ - Create users
+ - Edit users
+ - Delete users
+ - Show users
+ - Igangværende bruger oprettelse
+ - Sluttede oprettelse af bruger
+
+
+Register new user fields:
+ - Full name
+    * Generates userName from initials
+ - Title / Position
+    * Gives permission matching the title/position
+    * Creates signature for eMail
+    * Gives access to drives and folders matching the title/position
+    * Gives access to nearest printers
+ - Address
+ - Cellphone number
+
+ Get data from all platforms
+ - SQL
+ - AD
+
+ CheckList Step By Step
+ - Kommentar til bruger/check liste
+ - Grupper tildelt
+ - Klargøring af pc
+ - Profil klar
+ - Opret telefon (IPVision) sidste 2 måske 4 cifre (Nice to have: Auto tilføj til brugerens direktenummer)
+ - Exchange CMD's
+ - Dynamisk Esktra punkter, som kan flyttes til tjekliste
+ - Default checkList
+
+NY BRUGER:
+ - Standard grupper
+ - Afdelings grupper
+ - Postkassser
+ - Data fra ny bruger:
+    * For Navn
+    * efternavn
+    * Brugernavn initialer
+    * Direkte tlf (Hovednummer)
+    * Mobil nr.
+    * Afdeling
+    * Job titel DK
+    * Job titel ENG
+    * e-mail = brugernavn@skibskredit.dk
+    * Company (Danmarks Skibskredit A/S)
+    * Midlertidig kode, mens opsætningen laves
+        > Nulstil kode, når alt er klar.
+ - Rolle fordeling i:
+    * SLS
+    * Adaptiv
+    * BC
+ - Diverse
+ - Ekstra, hvad hver enekelt bruger skal have af fysiske enheder
+ - Søulke (Ja / Nej)
+ - Registrering af enheder, telefon (fremtid), pc m.m.
+    * Er enhed udleveret
+
+
+
+ OPSTILLING AF MENU
+ - Start ny bruger
+ - Igangværende ny bruger
+ - Sluttede ny bruger
+
+***/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
