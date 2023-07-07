8ea6f24848922470edd998e3751c22a742569f1e
UnknownRussian <37229726+UnknownRussian@users.noreply.github.com>
2023-07-07 20:41:24 +0200

    Update: Refactoring of code

--- src/main/java/org/example/objects/CreateUserOption.java
+++ src/main/java/org/example/objects/CreateUserOption.java
@@ -7,44 +7,32 @@ public class CreateUserOption {
     private JLabel label;
     private JRadioButton yesInput, noInput;
     private JTextField txtInput;
-    private JPanel yesNoPanel, leftPanel, rightPanel, optionPanel;
-
-    public int getHeight() {
-        return height;
-    }
-
+    private JPanel  leftPanel, rightPanel, optionPanel;
     private int height;
-
-    public boolean isText() {
-        return isText;
-    }
-
-    private boolean isText;
 
     public CreateUserOption(boolean isText, String labelText){
         label = new JLabel(labelText);
         optionPanel = new JPanel();
         leftPanel = new JPanel();
         rightPanel = new JPanel();
+
         height = 40;
+
         optionPanel.setLayout(new GridLayout(1,2));
         optionPanel.setPreferredSize(new Dimension(610,height));
-
 
         leftPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         leftPanel.add(label);
 
-        rightPanel.setLayout(new FlowLayout((FlowLayout.LEFT)));
-
         if(isText){
+            rightPanel.setLayout(new FlowLayout((FlowLayout.LEFT)));
             txtInput = new JTextField();
             txtInput.setPreferredSize(new Dimension(300,30));
             txtInput.setFont(new Font("Calibri",Font.PLAIN,20));
             rightPanel.add(txtInput);
         }else {
-            yesNoPanel = new JPanel();
-            yesNoPanel.setLayout(new GridLayout(1,2));
-            yesNoPanel.setPreferredSize(new Dimension(300,30));
+            rightPanel.setLayout(new GridLayout(1,2));
+            rightPanel.setPreferredSize(new Dimension(300,30));
             yesInput = new JRadioButton("Ja");
             noInput = new JRadioButton("Nej");
 
@@ -52,17 +40,19 @@ public class CreateUserOption {
             radioBtnGroup.add(yesInput);
             radioBtnGroup.add(noInput);
 
-            yesNoPanel.add(yesInput);
-            yesNoPanel.add(noInput);
-            rightPanel.add(yesNoPanel);
+            rightPanel.add(yesInput);
+            rightPanel.add(noInput);
         }
 
         optionPanel.add(leftPanel);
         optionPanel.add(rightPanel);
-        this.isText = isText;
     }
 
     public JPanel getOptionPanel() {
         return optionPanel;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
